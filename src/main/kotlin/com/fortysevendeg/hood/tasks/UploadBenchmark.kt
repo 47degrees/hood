85863c6a75d9521af221d98c72b4b73379ce9c30
AdrianRaFo <15971742+AdrianRaFo@users.noreply.github.com>
2019-05-03 19:04:59 +0200

    allow update multiple files

--- src/main/kotlin/com/fortysevendeg/hood/tasks/UploadBenchmark.kt
+++ src/main/kotlin/com/fortysevendeg/hood/tasks/UploadBenchmark.kt
@@ -1,7 +1,9 @@
 package com.fortysevendeg.hood.tasks
 
 import arrow.core.toOption
+import arrow.data.extensions.list.foldable.traverse_
 import arrow.effects.IO
+import arrow.effects.extensions.io.applicative.applicative
 import arrow.effects.extensions.io.fx.fx
 import arrow.effects.fix
 import com.fortysevendeg.hood.OutputFile
@@ -12,23 +14,49 @@ import com.fortysevendeg.hood.github.GithubCommitIntegration
 import org.gradle.api.DefaultTask
 import org.gradle.api.GradleException
 import org.gradle.api.tasks.Input
-import org.gradle.api.tasks.InputFile
+import org.gradle.api.tasks.InputFiles
 import org.gradle.api.tasks.TaskAction
 import java.io.File
 
 open class UploadBenchmark : DefaultTask() {
 
-  @get:InputFile
-  var benchmarkFile: File =
-    project.objects.fileProperty().asFile.getOrElse(File("benchmarks/master_benchmark.csv"))
+  @get:InputFiles
+  var benchmarkFiles: List<File> =
+    project.objects.listProperty(File::class.java).getOrElse(emptyList())
   @get:Input
-  var benchmarkDestinationFromProjectRoot: String =
-    project.objects.property(String::class.java).getOrElse("benchmarks/master_benchmark.csv")
+  var uploadDirectory: String =
+    project.objects.property(String::class.java).getOrElse("benchmarks")
   @get:Input
   var commitMessage: String =
     project.objects.property(String::class.java).getOrElse("Upload benchmark")
   @get:Input
   var token: String? = project.objects.property(String::class.java).orNull
+
+  private fun upload(info: GhInfo, branch: String, benchmarkFile: File): IO<Unit> = fx {
+
+    val destination = uploadDirectory + benchmarkFile.name
+    val fileSha = !GithubCommitIntegration.getFileSha(info, branch, destination)
+
+    val content = !OutputFile.readFileToBase64(benchmarkFile)
+
+    fileSha.fold({
+      val createCommit = GhCreateCommit(commitMessage, content, branch)
+      !GithubCommitIntegration.createFileCommit(
+        info,
+        destination,
+        createCommit
+      )
+    }, {
+      val updateCommit =
+        GhUpdateCommit(commitMessage, content, it.sha, branch)
+      !GithubCommitIntegration.updateFileCommit(
+        info,
+        destination,
+        updateCommit
+      )
+    })
+
+  }
 
   @TaskAction
   fun uploadBenchmark(): Unit = fx {
@@ -39,33 +67,15 @@ open class UploadBenchmark : DefaultTask() {
     val owner: String = slug.first()
     val repo: String = slug.last()
 
-    val (token: String) =
+    val (ghToken: String) =
       token.toOption()
         .fold({ raiseError<String>(GradleException("Error getting Github token")) }) { IO { it } }
 
-    val info = GhInfo(owner, repo, token)
+    val info = GhInfo(owner, repo, ghToken)
 
-    val fileSha =
-      !GithubCommitIntegration.getFileSha(info, branch, benchmarkDestinationFromProjectRoot)
-
-    val content = !OutputFile.readFileToBase64(benchmarkFile)
-
-    fileSha.fold({
-      val createCommit = GhCreateCommit(commitMessage, content, branch)
-      !GithubCommitIntegration.createFileCommit(
-        info,
-        benchmarkDestinationFromProjectRoot,
-        createCommit
-      )
-    }, {
-      val updateCommit =
-        GhUpdateCommit(commitMessage, content, it.sha, branch)
-      !GithubCommitIntegration.updateFileCommit(
-        info,
-        benchmarkDestinationFromProjectRoot,
-        updateCommit
-      )
-    })
+    !benchmarkFiles.traverse_(IO.applicative()) {
+      upload(info, branch, it)
+    }
 
   }.fix().unsafeRunSync()
 
