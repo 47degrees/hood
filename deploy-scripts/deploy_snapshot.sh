2755d7893825a307edf9c6902d9dd99aede3fdba
AdrianRaFo <15971742+AdrianRaFo@users.noreply.github.com>
2020-05-14 14:47:25 +0200

    not change this

--- deploy-scripts/deploy_snapshot.sh
+++ deploy-scripts/deploy_snapshot.sh
@@ -16,7 +16,6 @@ elif [ "$TRAVIS_BRANCH" != "$BRANCH" ]; then
 elif ! [[ "$VERSION_NAME" =~ $VERSION_PATTERN ]]; then
   echo "Skipping snapshot deployment '$VERSION_NAME': This is probably a pre-release build"
 else
-  ./gradlew publishPluginToMavenLocal
   ./gradlew bintrayUpload
   echo "Snapshot '$VERSION_NAME' deployed!"
 fi
