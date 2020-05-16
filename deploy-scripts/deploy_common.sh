aa8a0cbc49b73246c65c394dd03b464633c3ff73
Rachel M. Carmena <rachelcarmena@users.noreply.github.com>
2020-05-16 18:48:05 +0200

    Configuration: Just one PR for RELEASE deployments

--- deploy-scripts/deploy_common.sh
+++ deploy-scripts/deploy_common.sh
@@ -3,7 +3,7 @@ set -e
 
 function getProperty {
     PROP_KEY=$1
-    PROP_VALUE=`cat gradle.properties | grep "$PROP_KEY" | cut -d'=' -f2`
+    PROP_VALUE=`cat gradle.properties | grep -e "^$PROP_KEY=" | cut -d'=' -f2`
     echo $PROP_VALUE
 }
 
@@ -12,6 +12,12 @@ function fail {
     exit -1
 }
 
+
 SLUG="47degrees/hood"
 BRANCH="master"
+RELEASE_VERSION=$(getProperty "release_version")
+LATEST_PUBLISHED_VERSION=$(curl https://dl.bintray.com/47deg/hood/com/47deg/hood/maven-metadata.xml | grep latest | cut -d'>' -f2 | cut -d'<' -f1)
+if [ "$RELEASE_VERSION" != "$LATEST_PUBLISHED_VERSION" ]; then
+    sed -i "s/version.*/version=$RELEASE_VERSION/g" gradle.properties
+fi
 VERSION_NAME=$(getProperty "version")
