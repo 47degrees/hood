1b386c49a7ea0ad0b42b04a595c030f7f8f33b4e
Rachel M. Carmena <rachelcarmena@users.noreply.github.com>
2020-06-14 18:45:03 +0200

    Fix URL (replace Bintray by Gradle Plugins Portal)

--- deploy-scripts/deploy_common.sh
+++ deploy-scripts/deploy_common.sh
@@ -14,9 +14,14 @@ function fail {
 
 SLUG="47degrees/hood"
 BRANCH="master"
+
+#
+# If release_version isn't published in Gradle Plugin Portal, version value is replaced by release_version value
+#
 RELEASE_VERSION=$(getProperty "release_version")
-LATEST_PUBLISHED_VERSION=$(curl https://dl.bintray.com/47deg/hood/com/47deg/hood/maven-metadata.xml | grep latest | cut -d'>' -f2 | cut -d'<' -f1)
+LATEST_PUBLISHED_VERSION=$(curl https://plugins.gradle.org/m2/com/47deg/hood/maven-metadata.xml | grep latest | cut -d'>' -f2 | cut -d'<' -f1)
 if [ "$RELEASE_VERSION" != "$LATEST_PUBLISHED_VERSION" ]; then
     sed -i "s/version.*/version=$RELEASE_VERSION/g" gradle.properties
 fi
+
 VERSION_NAME=$(getProperty "version")
