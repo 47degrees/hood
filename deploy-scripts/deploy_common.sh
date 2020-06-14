dd11431aab6795c55ca8666e8630df854c4553e5
Rachel M. Carmena <rachelcarmena@users.noreply.github.com>
2020-06-14 19:04:20 +0200

    Configuration: Just one PR for RELEASE deployments (#59)
    * Configuration: Just one PR for RELEASE deployments
    
    * Remove extra line
    
    * Remove deployment in Bintray for releases
    
    * Add badge
    
    * Solve #16
    
    * Remove bintray plugin because of @AdrianRaFo comment
    
    * Fix URL (replace Bintray by Gradle Plugins Portal)
    
    * Add more details because of @AdrianRaFo comment
    
    * Format

--- deploy-scripts/deploy_common.sh
+++ deploy-scripts/deploy_common.sh
@@ -3,7 +3,7 @@ set -e
 
 function getProperty {
     PROP_KEY=$1
-    PROP_VALUE=`cat gradle.properties | grep "$PROP_KEY" | cut -d'=' -f2`
+    PROP_VALUE=`cat gradle.properties | grep -e "^$PROP_KEY=" | cut -d'=' -f2`
     echo $PROP_VALUE
 }
 
@@ -14,4 +14,14 @@ function fail {
 
 SLUG="47degrees/hood"
 BRANCH="master"
+
+#
+# If 'release_version' isn't published in Gradle Plugin Portal, 'version' value is replaced by 'release_version' value
+#
+RELEASE_VERSION=$(getProperty "release_version")
+LATEST_PUBLISHED_VERSION=$(curl https://plugins.gradle.org/m2/com/47deg/hood/maven-metadata.xml | grep latest | cut -d'>' -f2 | cut -d'<' -f1)
+if [ "$RELEASE_VERSION" != "$LATEST_PUBLISHED_VERSION" ]; then
+    sed -i "s/version.*/version=$RELEASE_VERSION/g" gradle.properties
+fi
+
 VERSION_NAME=$(getProperty "version")
